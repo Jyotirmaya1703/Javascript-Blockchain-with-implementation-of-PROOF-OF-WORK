6d5d1f422393df00e05572963b538334aa9f37f2
jyotirmaya mishra <jyotirmaya1707@gmail.com>
2022-07-11 16:08:07 +0530

    first commit

---
+++ dev/blockchain.js
@@ -0,0 +1,60 @@
+const sha256 = require('sha256')
+
+function Blockchain() {
+    this.chain = [];
+    this.newTransaction = [];
+
+    this.createNewBlock(100, '0', '0');
+}
+
+Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
+    const newBlock = {
+        index: this.chain.length+1,
+        timeStamp: Date.now(),
+        transactions: this.newTransaction,
+        nonce: nonce,
+        hash: hash,
+        previousBlockHash: previousBlockHash
+    };
+
+    this.newTransaction = [];
+    this.chain.push(newBlock);
+
+    return newBlock;
+}
+
+Blockchain.prototype.getLastBlock = function() {
+    return (this.chain.length-1);
+}
+
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) {
+    const newTransaction = {
+        amount: amount,
+        sender: sender,
+        recipient: recipient
+    };
+
+    this.newTransaction.push(newTransaction);
+
+    return this.getLastBlock()['index'] + 1;
+}
+
+Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
+    const dataaString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    const hash = sha256(dataaString);
+    return hash;
+}
+
+Blockchain.prototype.proofOWork = function(previousBlockHash, currentBlockData) {
+    let nonce = 0;
+    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    while (hash.substring(0 , 4) !== '0000') {
+        nonce++;
+        hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+        console.log(hash);
+    }
+
+    return nonce;
+}
+
+module.exports = Blockchain;
---
+++ dev/test.js
@@ -0,0 +1,26 @@
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+
+// const previousBlockHash = '0INAAEGYESIUGTIUWT489TUEIUTAUB'
+// const currentBlockData = [
+//     {
+//         amount: 10,
+//         sender: 'BEGEOSIGOSIGIOI4WT',
+//         recipient: '024VRFWKJAEGTUQTQG'
+//     },
+//     {
+//         amount: 20,
+//         sender: 'BEGEOnsffffSIGOSIGIOI4WT',
+//         recipient: '024VRFWKJgdtmmdAEGTUQTQG'
+//     },
+//     {
+//         amount: 30,
+//         sender: 'BEGEndfnSIGOSIGIOI4WT',
+//         recipient: '024VRFWKJngndmdAEGTUQTQG'
+//     }
+// ]
+ 
+// console.log(bitcoin.proofOWork(previousBlockHash, currentBlockData));
+
+console.log(bitcoin);
